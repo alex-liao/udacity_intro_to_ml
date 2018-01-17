42306379f23c6648319421b0be795469de63bd30
Alex Liao (RD-TW) <alex_liao@trend.com.tw>
2018-01-17 18:12:17 +0800

    [Mini Project] Text Learning, quiz 19: Clean Away "Signature Words"

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -48,20 +48,31 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            stemmed_string = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            signature_words = ["sara", "shackleton", "chris", "germani"]
+            for signature_word in signature_words:
+                stemmed_string.replace(signature_word, "")
 
             ### append the text to word_data
+            word_data.append(stemmed_string)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            elif name == "chris":
+                from_data.append(1)
 
             email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
+
+# output for word_data[152]
+print word_data[152]
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
